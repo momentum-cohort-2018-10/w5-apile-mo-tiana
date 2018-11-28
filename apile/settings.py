40c13a580c34bc4af93ee5116d7de47b04002999
Mo Glatz <meagabeth@icloud.com>
2018-11-28 11:05:06 -0500

    Working on heroku setup

--- apile/settings.py
+++ apile/settings.py
@@ -86,7 +86,10 @@ WSGI_APPLICATION = 'apile.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'apile',
+        'USER': 'apile',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
