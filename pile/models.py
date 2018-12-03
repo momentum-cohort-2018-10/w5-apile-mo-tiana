472e37d9208c5c2688fa1c8c175c508516f395f2
tiana-horn <tiana.horn@gmail.com>
2018-12-03 09:23:21 -0500

    More styling, edit comments

--- pile/models.py
+++ pile/models.py
@@ -30,7 +30,9 @@ class Comment(models.Model):
     post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name='comments')
     new_comment = models.TextField("", max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
-
+    
+    def __str__(self):
+        return self.new_comment
 
 class Favorite(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
